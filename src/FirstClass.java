52f93f9f46342798a07c3ba4b38ef66d0b7aed10
Ashutosh Rawat <ashutosh.rawat143@gmail.com>
2022-04-13 19:39:15 +0530

    M21

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -29,11 +29,6 @@ public class FirstClass {
         System.out.println("Rawat Ji");
     }
 
-          public static void M21(String[] args) {
-        System.out.println("Rawat");
-        System.out.println("Ashutosh Ji");
-        System.out.println("Rawat Ji");
-    }
         public static void M3(String[] args) {
         System.out.println("Rawat");
         System.out.println("Ashutosh Ji");
