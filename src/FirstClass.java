98afba6313485cefa2aa44004a46a6e8588a4045
Ashutosh Rawat <ashutosh.rawat143@gmail.com>
2022-04-13 19:32:17 +0530

    Feature/first feature (#9)
    * Test Method commit.
    
    * Class 2 updated
    
    * added comment line
    
    * Added the method test1
    
    * F1
    
    * F2
    
    * Merged Request
    
    * F11
    
    * F21
    
    Co-authored-by: ashutoshrawat <ashutosh.rawat@gojek.com>

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -17,13 +17,13 @@ public class FirstClass {
         System.out.println("Rawat Ji");
     }
 
-    public static void F1(String[] args) {
+    public static void F11(String[] args) {
         System.out.println("Rawat");
         System.out.println("Ashutosh Ji");
         System.out.println("Rawat Ji");
     }
 
-    public static void F2(String[] args) {
+    public static void F21(String[] args) {
         System.out.println("Rawat");
         System.out.println("Ashutosh Ji");
         System.out.println("Rawat Ji");
