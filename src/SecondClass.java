b530ca879d8c89c0b622167d72665d143ab12f0a
Ashutosh Rawat <ashutosh.rawat143@gmail.com>
2022-04-13 16:05:34 +0530

    Update SecondClass.java
    Added coment Ashutosh ji

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -6,6 +6,7 @@ public class SecondClass {
 
     public static void newMethod(String[] args) {
  System.out.println("Rawat");
+        System.out.println("Ashutosh Ji");
     }
     //Just for the confirmation of master
 }
