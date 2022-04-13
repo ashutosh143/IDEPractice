91dbe7d8e45c114963fa0b6530d1497d721a55d4
Ashutosh Rawat <ashutosh.rawat143@gmail.com>
2022-04-13 16:09:01 +0530

    Feature/first feature (#4)
    * Test Method commit.
    
    * Class 2 updated
    
    * added comment line
    
    Co-authored-by: ashutoshrawat <ashutosh.rawat@gojek.com>

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -7,6 +7,7 @@ public class SecondClass {
     public static void newMethod(String[] args) {
  System.out.println("Rawat");
         System.out.println("Ashutosh Ji");
+        System.out.println("Rawat Ji");
     }
     //Just for the confirmation of master
 }
