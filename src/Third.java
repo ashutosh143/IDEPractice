41aa5577ddec4bd87264d39bd850b26538a86525
ashutoshrawat <ashutosh.rawat@gojek.com>
2022-04-16 12:09:52 +0530

    remove line 2

--- src/Third.java
+++ src/Third.java
@@ -2,7 +2,7 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test");
-        System.out.println("Test");
+
 
 
     }
